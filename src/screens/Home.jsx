36aa5eda939b0aea3406283a98511ddceafe2d14
rinoBax <aderino123321@gmail.com>
2025-07-29 09:44:27 +0700

    update url download

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -60,7 +60,7 @@ export default function Home({ navigate }) {
                                     <p className="download-prompt-text">Unduh aplikasi kami sekarang</p>
                                     <div className="app-downloads">
                                         {/* https://drive.usercontent.google.com/download?id=1vNDJqd_6dZ4ifjZICKrgvXOsvKgxQ4XH&export=download&authuser=0&confirm=t&uuid=a0beaf99-58f5-4cf0-a4d2-66fe870dc838&at=AN8xHor1ZIbw6nAu1__Ku17tZddO:1752744119847 */}
-                                        <a href="baxlancerimagefile/uploads/berkas-1753027848904-195246689.apk" className="btn btn-download">
+                                        <a href="https://baxlancerimagefile.nos.wjv-1.neo.id/uploads/berkas-1753027848904-195246689.apk" className="btn btn-download">
                                             <AndroidIcon />
                                             <span>Android</span>
                                         </a>
